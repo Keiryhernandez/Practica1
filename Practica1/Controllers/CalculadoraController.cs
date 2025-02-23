8d8d058af312c867e6cbffd9eed416e9a4f0e140
Keiry <keyri2017hernadez@gmail.com>
2025-02-22 20:55:42 -0600

    Subida de practica

---
+++ Practica1/Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica1.Models;
+
+namespace Practica1.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Operaciones();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Operaciones model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.TipoOperacionId == 1) // Kilómetros a Millas
+                {
+                    model.Resultado = model.PrimerDato * 0.621371;
+                }
+                else if (model.TipoOperacionId == 2) // Millas a Kilómetros
+                {
+                    model.Resultado = model.PrimerDato / 0.621371;
+                }
+                else if (model.TipoOperacionId == 3) // Metros a Pies
+                {
+                    model.Resultado = model.PrimerDato * 3.28084;
+                }
+                else if (model.TipoOperacionId == 4) // Pies a Metros
+                {
+                    model.Resultado = model.PrimerDato / 3.28084;
+                }
+            }
+            return View(model);
+        }
+    }
+
+
+}
---
+++ Practica1/Models/Operaciones.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica1.Models
+{
+    public class Operaciones
+    {
+        [Required]
+        [Display(Name = "Primer Dato")]
+        public double PrimerDato { get; set; } = 0;
+
+
+        public double Resultado { get; set; }
+        public int TipoOperacionId { get; set; } = 0;
+        public List<TiposOperacion> ListaOperaciones { get; set; }
+        public Operaciones()
+        {
+            ListaOperaciones = new List<TiposOperacion>
+            {
+                new TiposOperacion{Id= 1, Nombre= "Kilometro a Millas "},
+                new TiposOperacion{Id= 2, Nombre= "Millas a Kilometros"},
+                new TiposOperacion{Id= 3, Nombre= "Metros a Pies"},
+                new TiposOperacion{Id= 4, Nombre= "Pies a metros"},
+            };
+        }
+
+    }
+}
